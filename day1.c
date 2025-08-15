313c2419bda4aa3ba559c94dd56d6b17e737e108
RiddhiRopalkar <riddhi75rop@gmail.com>
2025-08-15 23:11:42 +0530

    Update day1.c
    Day 1 submission

--- day1.c
+++ day1.c
@@ -1,1 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void sort(int arr[], int n) {
+    int low = 0, mid = 0, high = n - 1;
+    int temp;
+
+    while (mid <= high) {
+        if (arr[mid] == 0) {
+            temp = arr[low];
+            arr[low] = arr[mid];
+            arr[mid] = temp;
+            low++;
+            mid++;
+        }
+        else if (arr[mid] == 1) {
+            mid++;
+        }
+        else {
+            temp = arr[mid];
+            arr[mid] = arr[high];
+            arr[high] = temp;
+            high--;
+        }
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter the size of array: ");
+    scanf("%d", &n);
+
+    int* arr = (int*) malloc(n * sizeof(int));
+    if (arr == NULL) {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+    printf("Enter %d elements:\n", n);
+    for (int m = 0; m < n; m++) {
+        scanf("%d", &arr[m]);
+    }
+
+    printf("Original array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    sort(arr, n);
+
+    printf("\nSorted array:   ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    free(arr);
+    return 0;
+}
 
